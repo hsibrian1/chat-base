7b5e81bd9f904b4938432aabb38c3e22e4beffcc
Hemny Sibrian <sibrianhemny@gmail.com>
2021-08-17 02:04:52 -0400

    Minor changes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'sign-in' },
   { path: 'sign-in', loadChildren: () => import('./pages/sign-in/sign-in.module').then(m => m.SignInModule) },
-  { path: '**', redirectTo: 'sign-in' },
 ];
 
 @NgModule({
