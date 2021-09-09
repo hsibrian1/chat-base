7ec1cb79ce6bdbdfd11c3896f1fc3d16f88b83a2
Hemny Sibrian <sibrianhemny@gmail.com>
2021-09-09 17:08:45 -0400

    Change for testing.

--- api/chat/init.js
+++ api/chat/init.js
@@ -1,38 +1,36 @@
 import { StreamChat } from 'stream-chat'
 
-export default async (req, res) => {
+export default async function init(req, res) {
   const { userSender, userReceiver } = req.body
   try {
-    console.log(
-      userSender, userReceiver
-    )
-    const serverClient = StreamChat.getInstance(
-      process.env.STREAM_CHAT_API_KEY,
-      process.env.STREAM_CHAT_SECRET_KEY
-    )
-    const token = serverClient.createToken(userSender)
-    const response = await client.upsertUsers([
-      {
-        id: userSender,
-        role: 'admin',
-        mycustomfield: userSender
-      },
-      {
-        id: userReceiver,
-        role: 'receicer',
-        mycustomfield: userReceiver
-      }
-    ])
-    const channel = serverClient.channel(
-      `${userSender}-To-${userReceiver}`, // type
-      `${userSender}__${userReceiver}`, // Channel Id
-      {
-        name: 'Chat Base',
-        created_by: { id: userSender }
-      }
-    )
-    await channel.create()
-    await channel.addMembers([userReceiver, userSender])
+    console.log(userSender, userReceiver)
+    // const serverClient = StreamChat.getInstance(
+    //   process.env.STREAM_CHAT_API_KEY,
+    //   process.env.STREAM_CHAT_SECRET_KEY
+    // )
+    // const token = serverClient.createToken(userSender)
+    // const response = await client.upsertUsers([
+    //   {
+    //     id: userSender,
+    //     role: 'admin',
+    //     mycustomfield: userSender
+    //   },
+    //   {
+    //     id: userReceiver,
+    //     role: 'receicer',
+    //     mycustomfield: userReceiver
+    //   }
+    // ])
+    // const channel = serverClient.channel(
+    //   `${userSender}-To-${userReceiver}`, // type
+    //   `${userSender}__${userReceiver}`, // Channel Id
+    //   {
+    //     name: 'Chat Base',
+    //     created_by: { id: userSender }
+    //   }
+    // )
+    // await channel.create()
+    // await channel.addMembers([userReceiver, userSender])
     res.json({ message: 'Success!' })
   } catch (e) {
     console.error(e)
