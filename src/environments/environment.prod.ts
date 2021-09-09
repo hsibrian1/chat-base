f4cf81b82859e6860353b690275fe7cfd3fc0fc4
Hemny Sibrian <sibrianhemny@gmail.com>
2021-09-09 17:57:33 -0400

    Env production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  version: '0',
+  firebaseConfig: {
+    apiKey: 'AIzaSyAmwIhA7RIbSkve9lmFFsBBfVAjNI73wNo',
+    authDomain: 'chat-demo-ba610.firebaseapp.com',
+    projectId: 'chat-demo-ba610',
+    storageBucket: 'chat-demo-ba610.appspot.com',
+    messagingSenderId: '434561817965',
+    appId: '1:434561817965:web:32032fbb4080dd09e18e64'
+  },
+  chatStreamAPIKey: "fpdx7ab5gr6x",
 };
