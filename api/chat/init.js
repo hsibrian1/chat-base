ba8c1b79c241c80bea17b7c1fbfab4c89f956821
Hemny Sibrian <sibrianhemny@gmail.com>
2021-09-09 17:13:01 -0400

    Minor fix.

--- api/chat/init.js
+++ api/chat/init.js
@@ -4,33 +4,33 @@ export default async function init(req, res) {
   const { userSender, userReceiver } = req.body
   try {
     console.log(userSender, userReceiver)
-    // const serverClient = StreamChat.getInstance(
-    //   process.env.STREAM_CHAT_API_KEY,
-    //   process.env.STREAM_CHAT_SECRET_KEY
-    // )
-    // const token = serverClient.createToken(userSender)
-    // const response = await client.upsertUsers([
-    //   {
-    //     id: userSender,
-    //     role: 'admin',
-    //     mycustomfield: userSender
-    //   },
-    //   {
-    //     id: userReceiver,
-    //     role: 'receicer',
-    //     mycustomfield: userReceiver
-    //   }
-    // ])
-    // const channel = serverClient.channel(
-    //   `${userSender}-To-${userReceiver}`, // type
-    //   `${userSender}__${userReceiver}`, // Channel Id
-    //   {
-    //     name: 'Chat Base',
-    //     created_by: { id: userSender }
-    //   }
-    // )
-    // await channel.create()
-    // await channel.addMembers([userReceiver, userSender])
+    const serverClient = StreamChat.getInstance(
+      "fpdx7ab5gr6x",
+      "yrdtbb2cmcf42jad2b95vytnf9aym93dn3da3b9uuuqsg66hasundfseyc347dr5"
+    )
+    const token = serverClient.createToken(userSender)
+    const response = await serverClient.upsertUsers([
+      {
+        id: userSender,
+        role: 'admin',
+        mycustomfield: userSender
+      },
+      {
+        id: userReceiver,
+        role: 'receicer',
+        mycustomfield: userReceiver
+      }
+    ])
+    const channel = serverClient.channel(
+      `${userSender}-To-${userReceiver}`, // type
+      `${userSender}__${userReceiver}`, // Channel Id
+      {
+        name: 'Chat Base',
+        created_by: { id: userSender }
+      }
+    )
+    await channel.create()
+    await channel.addMembers([userReceiver, userSender])
     res.json({ message: 'Success!' })
   } catch (e) {
     console.error(e)
